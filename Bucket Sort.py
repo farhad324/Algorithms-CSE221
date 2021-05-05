6a7a3711a52f2858044844fc173b5967c48112b9
Farhadul Islam <59443923+farhad324@users.noreply.github.com>
2021-05-06 04:51:01 +0600

    Add files via upload

---
+++ Bucket Sort.py
@@ -0,0 +1,34 @@
+def insertionSort(element_array):
+    for i in range(1, len(element_array)):
+        key = element_array[i]
+        j = i - 1
+        while j >= 0 and element_array[j] > key:
+            element_array[j + 1] = element_array[j]
+            j -= 1
+        element_array[j + 1] = key    
+    return element_array
+
+def bucketSort(array):
+    bucket = []
+
+    for i in range(len(array)):
+        bucket.append([])
+
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+
+    for i in range(len(array)):
+        bucket[i] = insertionSort(bucket[i])
+
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+
+array = [.44, .39, .33, .21, .51, .47, .51]
+sorted_array = bucketSort(array)
+print("Sorted Array:",sorted_array)
