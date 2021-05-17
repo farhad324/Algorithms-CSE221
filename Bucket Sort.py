fe679740c114b2c49dab0a1fbf1f4b6990e4e2f9
Farhadul Islam <59443923+farhad324@users.noreply.github.com>
2021-05-17 23:33:57 +0600

    Update Bucket Sort.py

--- Bucket Sort.py
+++ Bucket Sort.py
@@ -1,41 +1,41 @@
-def insertionSort(element_array):
-    for i in range(1, len(element_array)):
-        key = element_array[i]
+def insertionSort(b):
+    for i in range(1, len(b)):
+        up = b[i]
         j = i - 1
-        while j >= 0 and element_array[j] > key:
-            element_array[j + 1] = element_array[j]
+        while j >= 0 and b[j] > up:
+            b[j + 1] = b[j]
             j -= 1
-        element_array[j + 1] = key    
-    return element_array
+        b[j + 1] = up    
+    return b    
+             
+def bucketSort(A):
+    n=len(A)
+    arr = []
+    for i in range(n):
+        arr.append([])    
+    for i in range(0,n):
+        arr[int(n*A[i])].append(A[i])
 
-def bucketSort(array):
-    bucket = []
-
-    for i in range(len(array)):
-        bucket.append([])
-
-    for j in array:
-        index_b = int(10 * j)
-        bucket[index_b].append(j)
-
-    for i in range(len(array)):
-        bucket[i] = insertionSort(bucket[i])
-
+    for i in range(0,n):
+        arr[i] = insertionSort(arr[i])
+   
+       
     k = 0
-    for i in range(len(array)):
-        for j in range(len(bucket[i])):
-            array[k] = bucket[i][j]
+    for i in range(n):
+        for j in range(len(arr[i])):
+            A[k] = arr[i][j]
             k += 1
-    return array
+    return A
+ 
 
+x= [0.77,0.16,0.39,0.26,0.71,0.95,0.21,0.12,0.23,0.68]
 
-array = [.44, .39, .33, .21, .51, .47, .51]
-sorted_array = bucketSort(array)
-print("Sorted Array:",sorted_array)
+print("Sorted Array is:",bucketSort(x))
+
 
 ## OUTPUT:
 
 '''
-Sorted Array: [0.21, 0.33, 0.39, 0.44, 0.47, 0.51, 0.51]
+Sorted Array is: [0.12, 0.16, 0.21, 0.23, 0.26, 0.39, 0.68, 0.71, 0.77, 0.95]
 
 '''
